fefd02bb0b3ab13c4cc49222accc8425521feb44
mukeem mohd <mohdmukeem9971@gmail.com>
2026-03-24 18:10:20 +0530

    Revert to bundler moduleResolution: better for monorepos with TypeScript sources

--- lib/db/dist/index.d.ts
+++ lib/db/dist/index.d.ts
@@ -1,5 +1,6 @@
+import * as schema from "./schema";
 export declare const pool: import("pg").Pool;
-export declare const db: import("drizzle-orm/node-postgres").NodePgDatabase<any> & {
+export declare const db: import("drizzle-orm/node-postgres").NodePgDatabase<typeof schema> & {
     $client: import("pg").Pool;
 };
 export * from "./schema";
